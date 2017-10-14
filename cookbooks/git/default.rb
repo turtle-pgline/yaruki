dca815d4c10c076ad469c99b6ec967f0ea1b3af7
naari3 <naari.named@gmail.com>
2017-10-15 02:55:38 +0900

    add git

---
+++ cookbooks/git/default.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+package 'git' do
+  action :install
+end
--- roles/default.rb
+++ roles/default.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 include_recipe '../cookbooks/htop/default'
+include_recipe '../cookbooks/git/default'
 include_recipe '../cookbooks/users/default'
