7ddceab2248b34a68f1971612dd6d8205cf14859
naari3 <naari.named@gmail.com>
2017-10-15 03:20:36 +0900

    add r

--- cookbooks/users/default.rb
+++ cookbooks/users/default.rb
@@ -5,7 +5,8 @@ require 'unix_crypt'
 {
   # username => github name
   'yuki_sone' => 'naari3',
-  'mox' => 'moezakura'
+  'mox' => 'moezakura',
+  'r' => 'ryota-sakamoto'
 }.each_pair do |name, github|
   user name do
     action :create
